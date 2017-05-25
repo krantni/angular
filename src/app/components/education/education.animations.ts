82dd5d1a3ffc0d5502a6b468ac70cfe11a6d50f6
Nick Krantz <krantni@gmail.com>
2017-05-24 22:22:34 -0500

    improving animations

--- src/app/components/education/education.animations.ts
+++ src/app/components/education/education.animations.ts
@@ -3,14 +3,10 @@ import {  trigger,  state,  style,  animate,  transition, group} from '@angular/
 export const animateSchool = trigger('initState', [
   state('active', style({opacity: 1, transform: 'translateX(0)'})),
   transition('void => *', [
-    style({transform: 'translateX(-100%)', opacity: 0}),
-        group([
-          animate('1s ease', style({
-            transform: 'translateX(0)',
-          })),
-          animate('1.5s ease', style({
-            opacity: 1
-          }))
-        ])
-      ])
+    style({transform: 'translateX(100%)', opacity: 0}),
+      animate('1s ease', style({
+        transform: 'translateX(0)',
+        opacity: 1
+      }))
+    ])
 ]);
--- src/app/components/experience/experience.animations.ts
+++ src/app/components/experience/experience.animations.ts
@@ -4,13 +4,9 @@ export const animateJob = trigger('initState', [
   state('active', style({opacity: 1, transform: 'translateX(0)'})),
   transition('void => *', [
     style({transform: 'translateX(-100%)', opacity: 0}),
-        group([
-          animate('1s ease', style({
-            transform: 'translateX(0)',
-          })),
-          animate('1.5s ease', style({
-            opacity: 1
-          }))
-        ])
-      ])
+    animate('1s ease', style({
+      transform: 'translateX(0)',
+      opacity: 1
+    }))
+  ])
 ]);
