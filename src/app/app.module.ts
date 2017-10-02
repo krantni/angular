7b868bf1f44bbebb49217101e7f540b9cc265ec2
krantni <krantni@gmail.com>
2017-10-01 22:46:54 -0500

    removing hash from URLs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,7 @@ const appRoutes: Routes = [
     BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes,{useHash:true})
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [GoogleAnalyticsEventsService],
   bootstrap: [AppComponent]
