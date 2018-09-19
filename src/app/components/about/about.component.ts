b38bf55e4c2b71561364a52996ad7f8e9bf89d74
krantni <krantni@gmail.com>
2018-09-18 22:02:19 -0500

    Spelling and home content

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -9,8 +9,8 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
-  skills:string = "Throughout my education and work experience I have been exposed to many languages: JavaScript, HTML, CSS, NodeJS, Java and C++ to name a few. You can look through my education and experience pages to learn more about my skillset.";
-  about:string = "Hello! I built this website to have a personal resume website online using Node, Angular, Bootstrap, and Firebase NoSQL database. It is a work in progress (as most things are) but the code for this website can be found on my GitHub, or to get in touch go through any of the social sites below!";
+  skills:string = "Throughout my education and work experience I have been exposed to many languages: JavaScript, HTML, CSS, NodeJS, React + Redux, Backbone, Java and C++ to name a few. You can look through my education and experience pages to learn more about my skillset and experiences. ";
+  about:string = "Hello! I built this website using Angular and Firebase NoSQL database. It is a work in progress (as most things are) but the code for this website can be found on my GitHub. Feel free to take a look at my past education, experience and even some recipes that I needed place to store. If you'd like to get in touch go feel free to shoot me an email at krantznicholas@gmail.com!";
 
   ngOnInit() {
   }
